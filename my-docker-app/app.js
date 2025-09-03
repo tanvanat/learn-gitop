b436b4f0f0743e139114960ed3205c75fd4dbb9f
tanvanat <tanvanat112@gmail.com>
2025-09-03 16:58:18 +0700

    test CI change

--- my-docker-app/app.js
+++ my-docker-app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Hello from CI/CD test v1'); // เปลี่ยนข้อความนี้เวลาทดสอบรอบต่อไป
+  res.send('Hello from CI/CD test v2'); // เปลี่ยนข้อความนี้เวลาทดสอบรอบต่อไป
 });
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}`));
