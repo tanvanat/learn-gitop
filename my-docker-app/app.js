3c3e8853b01e71b8d28a85bbc4f04e9e95a70a78
tanvanat <tanvanat112@gmail.com>
2025-09-03 17:07:02 +0700

    test: change message to v3

--- my-docker-app/app.js
+++ my-docker-app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Hello from CI/CD test v2'); // เปลี่ยนข้อความนี้เวลาทดสอบรอบต่อไป
+  res.send('Hello from CI/CD test v3'); // เปลี่ยนข้อความนี้เวลาทดสอบรอบต่อไป
 });
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}`));
